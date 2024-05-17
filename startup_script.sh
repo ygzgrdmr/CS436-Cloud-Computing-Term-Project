209b57c9678518fdbff7f944884242ec646ee315
Yağız Gürdamar <yagiz.gurdamar@sabanciuniv.edu>
2024-05-17 12:12:44 +0300

    Add startup_script.sh to the repository

---
+++ startup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Güncellemeler ve gerekli paketlerin kurulumu
+sudo apt update
+sudo apt install -y git docker.io python3 python3-pip
+
+# Docker servislerinin başlatılması
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
+cd CS436-Term-Project/infinity-search-solo
+
+# Python bağımlılıklarının kurulumu
+pip3 install -r requirements.txt
+pip3 install --upgrade jinja2 flask
+
+# Docker image'ının oluşturulması ve çalıştırılması
+docker build -t my-python-app .
+docker run -p 5000:5000 -d my-python-app
